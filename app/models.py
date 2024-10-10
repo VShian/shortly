651aa0c6b8a084e752d7caf923a0820491ddc24b
vshian <vvardhanreddy4@gmail.com>
2024-10-10 15:21:21 +0530

    adds model ShortURL

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ShortURL(models.Model):
+    short_key = models.CharField(max_length=100, primary_key=True)
+    url = models.URLField(max_length=2048, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.short_key
